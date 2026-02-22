c67c6fec6513736197bda92867eb3fdacda91be1
Felipe Yunoki <f.kenjiyunoki@gmail.com>
2026-02-22 16:59:37 -0300

    feat: add initial header structure (HTML, CSS and JS)

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
